37a14a6b0a3cbd0be66322c40f682f3707fa2cb3
Alex Duka <olekduka@amazon.com>
2020-12-14 19:07:42 +0000

    07 DOM

---
+++ 04/14.js
@@ -0,0 +1,4 @@
+let arr = [1, 3, 3, 4, 5, 6, 6, 6, 8, 4, 1]
+let unique_numbers = [...new Set(arr)];
+console.log(unique_numbers);
+// [1,3, 4, 5, 6, 8]
--- 04/ex9.js
+++ 04/ex9.js
@@ -17,13 +17,35 @@ const users = require('./users');
 //
 // console.log(activeUsers);
 
-const uniqueSkills = Object.keys(users.reduce((skills, user) => {
+// const uniqueSkills = Object.keys(users.reduce((skills, user) => {
+//   user.skills.forEach(skill => {
+//     skills[skill] = true;
+//   })
+//
+//   return skills;
+// }, {})).sort();
+
+
+const uniqueSkills = users.reduce((skills, user) => {
   user.skills.forEach(skill => {
-    skills[skill] = true;
+    skills.add(skill);
   })
 
   return skills;
-}, {})).sort();
+}, new Set());
+
+
+console.log(uniqueSkills.get('ipsum'))
+
+// let arr = [1, 3, 3, 4, 5, 6, 6, 6, 8, 4, 1]
+// const arr2 = arr;
+//
+// console.log(arr === arr2);
+//
+// let unique_numbers = [...new Set(arr)];
+// console.log(unique_numbers);
+// [1,3, 4, 5, 6, 8]
+
 
 console.log(uniqueSkills);
 
